c9a5dde09d633f60fa48363140b451422c5573a9
Conor Stephens <conor.p.stephens@ibm.com>
2018-04-19 01:18:11 -0500

    Added capability to GetUsers class to  download all pages of users at once and display them together

--- GPTestApp/app/src/main/java/com/example/conorpstephens/gptestapp/MainActivity.java
+++ GPTestApp/app/src/main/java/com/example/conorpstephens/gptestapp/MainActivity.java
@@ -1,28 +1,175 @@
 package com.example.conorpstephens.gptestapp;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView mRecyclerView;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ArrayList<JSONObject> users;
+
+    private RecyclerView mRecyclerView;
+    private UserAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initRecyclerView();
+        new GetUsers().execute();
     }
 
     private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView");
         mRecyclerView = findViewById(R.id.recyclerview_main);
 
-        LinearLayoutManager llm = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(OrientationHelper.VERTICAL);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(llm);
-
     }
 
+    public static ArrayList<JSONObject> getListOfJsonObject(
+            JSONArray jsonArray) {
+        ArrayList<JSONObject> list = new ArrayList<>();
 
+        if (jsonArray != null && jsonArray.length() > 0) {
+
+            for (int index = 0; index < jsonArray.length(); index++) {
+                JSONObject jsonObject = null;
+
+                try {
+                    jsonObject = jsonArray.getJSONObject(index);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (jsonObject != null) {
+                    list.add(jsonObject);
+                }
+            }
+        }
+        return list;
+    }
+
+    private class GetUsers extends AsyncTask<String, Integer, JSONArray> {
+        private ProgressDialog mProgressDialog;
+        private String urlString;
+
+        GetUsers() {
+            this.urlString = "https://reqres.in/api/users";
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create progress dialog
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            // Set your progress dialog Title
+            mProgressDialog.setTitle("Downloading");
+            // Set your progress dialog Message
+            mProgressDialog.setMessage("Please Wait!");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            // Show progress dialog
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected JSONArray doInBackground(String... Url) {
+            String result;
+            int pages = 1;
+            JSONObject jsonObject = null;
+            JSONArray jsonArray = new JSONArray();
+            result = downloadUrl(urlString);
+            try {
+                jsonObject = new JSONObject(result);
+                pages = jsonObject.getInt("total_pages");
+                jsonArray = jsonObject.optJSONArray("data");
+                Log.d(TAG, "JSONARRAY1: " + jsonArray.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            for(int i = 2; i <= pages; i++){
+                result = downloadUrl(urlString + "?page=" + i);
+                try {
+                    jsonObject = new JSONObject(result);
+                    JSONArray tempJsonArray = jsonObject.optJSONArray("data");
+                    Log.d(TAG, "TEMPJSONARRAY: " + tempJsonArray.toString());
+                    Log.d(TAG, "JSONARRAY: " +jsonArray.toString());
+
+                    for(int j=1; j<tempJsonArray.length();j++){
+                        jsonArray.put(tempJsonArray.get(j));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return jsonArray;
+        }
+
+        private String downloadUrl(String myurl){
+            BufferedReader reader;
+            try {
+                URL url = new URL(myurl);
+                HttpsURLConnection connection = (HttpsURLConnection)
+                        url.openConnection();  //HTTP connection is open
+                StringBuilder sb = new StringBuilder();
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while((line = reader.readLine())!= null){
+                    sb.append(line);
+                }
+                return sb.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray result) {
+            mProgressDialog.dismiss();
+            Log.d(TAG, "Result: " + result);
+
+            JSONObject jsonObject = null;
+
+//            try {
+//                jsonObject = new JSONObject(result);
+//
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            JSONArray jsonArray = jsonObject.optJSONArray("data");
+            users = getListOfJsonObject(result);
+            userAdapter = new UserAdapter(MainActivity.this,users);
+            Log.d(TAG, "setting adapter");
+            mRecyclerView.setAdapter(userAdapter);
+        }
+    }
 }
--- GPTestApp/app/src/main/java/com/example/conorpstephens/gptestapp/UserAdapter.java
+++ GPTestApp/app/src/main/java/com/example/conorpstephens/gptestapp/UserAdapter.java
@@ -1,27 +1,50 @@
 package com.example.conorpstephens.gptestapp;
 
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by conor.p.stephens@ibm.com on 4/18/18.
  */
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> implements View.OnClickListener {
+    private static final String TAG = UserAdapter.class.getSimpleName();
 
     private ArrayList<JSONObject> users;
     private Context activity;
 
     public UserAdapter(Context context, ArrayList<JSONObject> objects) {
+        Log.d(TAG, "Adapter Constructor");
         this.users = objects;
         this.activity = context;
     }
@@ -31,25 +54,36 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> im
         TextView userNameView;
         ImageView userPhotoView;
 
-
-
         ViewHolder(View itemView) {
             super(itemView);
-            cardView = (CardView) itemView.findViewById(R.id.cardview_user);
-            userPhotoView = (ImageView) itemView.findViewById(R.id.imageview_user_photo);
-            userNameView = (TextView) itemView.findViewById(R.id.textview_user_name);
+            cardView = itemView.findViewById(R.id.cardview_user);
+            userPhotoView = itemView.findViewById(R.id.imageview_user_photo);
+            userNameView = itemView.findViewById(R.id.textview_user_name);
         }
 
     }
 
     @Override
-    public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int index) {
+        Log.d(TAG, "onCreateViewHolder");
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_main_user_card, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(UserAdapter.ViewHolder holder, int position) {
+        JSONObject jsonObject = users.get(position);
 
+        Log.d(TAG, "onBindViewHolder");
+
+        try {
+            Log.d(TAG, jsonObject.getString("first_name"));
+            holder.userNameView.setText(jsonObject.getString("first_name") + " " + jsonObject.getString("last_name"));
+            new DownloadImageTask(holder.userPhotoView).execute(jsonObject.getString("avatar"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -59,6 +93,30 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> im
 
     @Override
     public int getItemCount() {
-        return 0;
+        return users.size();
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
